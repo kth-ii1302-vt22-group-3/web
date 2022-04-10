cafc442c4a059ef7e79b57b305c68a4b05e109b0
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-04-10 14:55:23 +0200

    First try for HomePagePresenter purpose adding picture to view

--- src/presenters/homePagePresenter.js
+++ src/presenters/homePagePresenter.js
@@ -1,24 +1,14 @@
 
 function HomePagePresenter(props){
-    const [dishes, setDishes]=React.useState(props.model.dishes);
-    const [number, setNumber]=React.useState(props.model.numberOfGuests);
-    const [username, setUsername]=React.useState(props.model.username);
+    const [picture, setPicture]=React.useState(props.model.picture);
 
-    React.useEffect( function(){
+    React.useEffect(function(){
         function obs(){
-            setUsername(props.model.username)
-            setNumber(props.model.numberOfGuests)
-            setDishes(props.model.dishes);}
+            setPicture(props.model.picture); 
+        }
         props.model.addObserver(obs);                               // 1. subscribe
         return function(){ props.model.removeObserver(obs);}        // 2.unsubscribe
-    }, []
-    )
+    }, [])
 
-    return <HomePageView persons={number}
-						startQuiz={()=>{props.model.startNewQuiz()}}
-                        onSignOut = {() => props.model.signOut()}
-                        areWeLoggedIn = {() => props.model.areWeLoggedIn()}
-                         username={username}
-                         clearError = {()=>props.model.resetError()}
-            />
+    return <HomePageView picture={picture}/>
 }
