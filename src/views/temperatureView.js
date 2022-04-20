f29179bf4823d59a0cd8a936beb8709978c51afa
carrogross <caroline.liljenstrom@gmail.com>
2022-04-20 15:00:47 +0200

    #WebDesign Must/Should view

---
+++ src/views/temperatureView.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+
+</head>
+<body>
+    <div class="HomePage">
+        <div class="Centered">
+            <p class="Header">The current temperature is: </p>
+            <p class="TimeStamp">Last updated: 2022-04-04 16:41</p>
+            <p class="Temperature">21°c</p>
+        </div>
+    </div>
+</body>
+</html>
