84b1012a5230684d95d2b05ee96e9b019c34e440
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-05-04 15:37:10 +0200

    #12: refactoring of homeÅagePresenter

--- src/presenters/homePagePresenter.js
+++ src/presenters/homePagePresenter.js
@@ -5,18 +5,6 @@ function HomePagePresenter(props){
     const [temper, setTemper] = React.useState(props.model.temperature)
     const [dateTime, setDateTime] = React.useState(props.model.timeStamp)
 
-    React.useEffect(function(){
-        function obs(){
-                setTemper(props.model.temperature)
-                setDateTime(props.model.timeStamp)
-                props.model.addObserver(obs);
-                return function(){
-                    props.model.removeObserver(obs);
-            }
-        }
-    }, []
-    
-    )
 
     return(
         <>
