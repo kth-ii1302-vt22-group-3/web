7e5baddbca29fa7c7cfdf141c5dfa0d6c00a19cf
arifjehoh <arifjeh.oh@gmail.com>
2022-05-05 21:40:59 +0200

    #22: Refactored and created function for chartdata

--- src/views/temperatureGraphView.js
+++ src/views/temperatureGraphView.js
@@ -2,63 +2,66 @@
 import { Chart as chartJS } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 
-function chartOptions(){
-    return {
-        //Make the size of graph to the same as container
-        responsive: true,
-        plugins: {
-          title: { display: true, text: "The Previous Temperature was:" },
+function chartOptions() {
+  return {
+    responsive: true,
+    plugins: {
+      title: { display: true, text: "The Previous Temperature was:" },
+    },
+
+    scales: {
+      x: {
+        grid: {
+          display: false,
         },
-    
-        scales: {
-          x: {
-            grid: {
-              display: false,
-            },
-            title: {
-              display: true,
-              text: "Day of the month",
-            },
-          },
-          y: {
-            grid: {
-              display: false,
-            },
-            ticks: {
-              callback: function (value, index, ticks) {
-                return value + "°c";
-              },
-            },
-            title: {
-              display: true,
-              text: "Temperature",
-            },
+        title: {
+          display: true,
+          text: "Day of the month",
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value) {
+            return value + " °c";
           },
         },
-      };
+        title: {
+          display: true,
+          text: "Temperature",
+        },
+      },
+    },
+  };
 }
 
-function TemperatureGraphView(props) {
-    //const{labels, dataset} = props.chartData
-  
-    const chartData = {
-    labels: props.labels,
+function chartData({ labels, dataset }) {
+  return {
+    labels: labels,
     datasets: [
       {
         label: "Temperature",
-        data: props.dataset,
+        data: dataset,
         borderColor: ["rgba(0, 0, 0, 0.6)"],
-        //fill: false,
         borderWidth: 4,
         tension: 0.1,
         pointStyle: "circle",
       },
     ],
   };
-  
+}
+
+function TemperatureGraphView(props) {
   return (
     <div style={{ width: 600 }}>
-      <Line data={chartData} height={400} width={600} options={chartOptions()} />
+      <Line
+        data={chartData(props.chartData)}
+        height={400}
+        width={600}
+        options={chartOptions()}
+      />
     </div>
   );
 }
