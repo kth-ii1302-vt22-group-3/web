f4c7f3809fee979825e504cb6689678ac9bed797
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-05-05 21:25:40 +0200

    #22: refactor to dynamic graph

--- src/views/temperatureGraphView.js
+++ src/views/temperatureGraphView.js
@@ -1,67 +1,66 @@
 // eslint-disable-next-line
-import {Chart as chartJS} from 'chart.js/auto' 
-import { Line } from 'react-chartjs-2'
+import { Chart as chartJS } from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+
+function chartOptions(){
+    return {
+        //Make the size of graph to the same as container
+        responsive: true,
+        plugins: {
+          title: { display: true, text: "The Previous Temperature was:" },
+        },
+    
+        scales: {
+          x: {
+            grid: {
+              display: false,
+            },
+            title: {
+              display: true,
+              text: "Day of the month",
+            },
+          },
+          y: {
+            grid: {
+              display: false,
+            },
+            ticks: {
+              callback: function (value, index, ticks) {
+                return value + "°c";
+              },
+            },
+            title: {
+              display: true,
+              text: "Temperature",
+            },
+          },
+        },
+      };
+}
 
 function TemperatureGraphView(props) {
-    return (
-        <div style = {{width:400}}>
-            <Line
-            data={{
-                labels: ["29/4", "30/4","1/5","2/5","3/5","4/5","5/5"],
-                datasets:[{
-                    label: 'Temperature',
-                    data: props.data,
-                    borderColor: [
-                        'rgba(0, 0, 0, 0.6)'
-                    ],
-                    //fill: false,
-                    borderWidth: 4,
-                    tension: 0.1,
-                    pointStyle: 'circle',
-                }]
-            }}
-            height={400}
-            width={600}
-            options = {{
-                //Make the size of graph to the same as container
-                responsive: true,
-                plugins: {
-                    title: {display: true, 
-                        text: "The Previous Temperature was:" 
-                    }
-                },
-                
-                scales: {
-                    x: {
-                      grid: {
-                        display: false,
-                      },
-                      title: {
-                          display:true,
-                          text:'Day of the month'
-                      }
-                    },
-                    y:{
-                        grid: {
-                            display: false,
-                        },
-                        ticks:{
-                            callback: function(value, index, ticks){
-                                return value+"°c";
-                            },
-                        },
-                        title: {
-                            display:true,
-                            text:'Temperature'
-                        }
-                         
-                    }
-                } 
-            }}
-            
-            />
-        </div>
-    );
+    //const{labels, dataset} = props.chartData
+  
+    const chartData = {
+    labels: props.labels,
+    datasets: [
+      {
+        label: "Temperature",
+        data: props.dataset,
+        borderColor: ["rgba(0, 0, 0, 0.6)"],
+        //fill: false,
+        borderWidth: 4,
+        tension: 0.1,
+        pointStyle: "circle",
+      },
+    ],
+  };
+  
+  return (
+    <div style={{ width: 600 }}>
+      <Line data={chartData} height={400} width={600} options={chartOptions()} />
+    </div>
+  );
 }
 
 export default TemperatureGraphView;
