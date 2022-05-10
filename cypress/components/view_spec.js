a438dec876de9e864fb49e7e37ed11aefc9e0e4b
arifjehoh <arifjeh.oh@gmail.com>
2022-05-10 21:27:30 +0200

    #35: Refactored component testing

---
+++ cypress/components/view_spec.js
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import { mount } from '@cypress/react';
+import Model from '../../src/model/model';
+import TemperatureView from "../../src/views/temperatureView";
+
+describe("Test case: Test the view-component with different data", () => {
+    it("Test scenario 1: View-component with default value", () => {
+        const model = new Model();
+        mount(<TemperatureView temperature={model.getTemperature()} timestamp={model.getTimestamp()} />);
+        cy.get("#current_temperature").contains("0");
+        cy.get("#current_timestamp").contains("1970-01-01 00:00:00");
+    });
+
+    it("Test scenario 2: View-component with temperature = 100, and timestamp = '1000-10-10 10:10:10'", () => {
+        const model = new Model();
+        model.setTemperature(100);
+        model.setTimestamp("1000-10-10 10:10:10");
+        mount(<TemperatureView temperature={model.getTemperature()} timestamp={model.getTimestamp()} />);
+        cy.get("#current_temperature").contains("100");
+        cy.get("#current_timestamp").contains("1000-10-10 10:10:10");
+    });
+
+    it("Test scenario 3: View-component with temperature = -100", () => {
+        const model = new Model();
+        model.setTemperature(-100);
+        mount(<TemperatureView temperature={model.getTemperature()} timestamp={model.getTimestamp()} />);
+        cy.get("#current_temperature").contains("-100");
+        cy.get("#current_timestamp").contains("1970-01-01 00:00:00");
+    });
+
+    it("Test scenario 4: View-component with temperature = 0", () => {
+        const model = new Model();
+        model.setTemperature(0);
+        mount(<TemperatureView temperature={model.getTemperature()} timestamp={model.getTimestamp()} />);
+        cy.get("#current_temperature").contains("0");
+        cy.get("#current_timestamp").contains("1970-01-01 00:00:00");
+    });
+
+    it("Test scenario 5: View-component with temperature = null, and timestamp = null", () => {
+        const model = new Model();
+        model.setTemperature(null);
+        model.setTimestamp(null);
+        mount(<TemperatureView temperature={model.getTemperature()} timestamp={model.getTimestamp()} />);
+        cy.get("#current_temperature").should("not.exist");
+        cy.get("#current_timestamp").should("not.exist");
+    });
+
+    it("Test scenario 6: View-component with temperature = undefined, and timestamp = undefined", () => {
+        const model = new Model();
+        model.setTemperature(undefined);
+        model.setTimestamp(undefined);
+        mount(<TemperatureView temperature={model.getTemperature()} timestamp={model.getTimestamp()} />);
+        cy.get("#current_temperature").should("not.exist");
+        cy.get("#current_timestamp").should("not.exist");
+    });
+
+    it("Test scenario 7: View-component with temperature = NaN, and timestamp = 123", () => {
+        const model = new Model();
+        model.setTemperature(NaN);
+        model.setTimestamp(123);
+        mount(<TemperatureView temperature={model.getTemperature()} timestamp={model.getTimestamp()} />);
+        cy.get("#current_temperature").should("not.exist");
+        cy.get("#current_timestamp").should("not.exist");
+    });
+
+    it("Test scenario 8: View-component with temperature = '100', and timestamp = 123", () => {
+        const model = new Model();
+        model.setTemperature('100');
+        model.setTimestamp(123);
+        mount(<TemperatureView temperature={model.getTemperature()} timestamp={model.getTimestamp()} />);
+        cy.get("#current_temperature").should("not.exist");
+        cy.get("#current_timestamp").should("not.exist");
+    });
+});
