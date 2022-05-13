f07b5c6980d67f5f0f366d26ae4c04e72846914c
carrogross <caroline.liljenstrom@gmail.com>
2022-05-13 10:30:41 +0200

    #34: Initialize Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
 import './App.css';
 import Model from './model/model';
 import HomePagePresenter from './presenters/homePagePresenter';
+import TemperatureView from "./views/temperatureView";
+import TemperatureGraphView from "./views/temperatureGraphView";
+import SidebarView from "./views/sidebarView";
+
 
 function App() {
   const model = new Model();
@@ -8,7 +13,17 @@ function App() {
   model.getLatests();
   return (
     <div className="App">
-      <HomePagePresenter model = {model}/>
+      <BrowserRouter>
+      <SidebarView />
+        <Routes>
+          <Route path="/" element={<Outlet/>}>
+            <Route index path="today" element={<TemperatureView temperature={0} timestamp={"dofhgjl"} />}/>
+            <Route path="graph" element={<TemperatureGraphView chartData={model.chartData} />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      {/* <HomePagePresenter model = {model}/> */}
     </div>
   );
 }
