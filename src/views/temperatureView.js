9a10c6e81dec5e25dfb7c8be7efbecdb7edf2911
carrogross <caroline.liljenstrom@gmail.com>
2022-05-14 00:09:14 +0200

    #34: refactor to specify css

--- src/views/temperatureView.js
+++ src/views/temperatureView.js
@@ -1,9 +1,8 @@
-
 function TemperatureView(props) {
     return (
         <div className="HomePage">
-            <div className="Centered">
-                <p className="Header">The current temperature is: </p>
+            <div className="TemperaturePosition">
+                <p className="TemperatureHeader">The current temperature is: </p>
                 {typeof(props.timestamp) === "string" && <p id='timestamp' className="Timestamp">Last updated: {props.timestamp}</p>}
                 {typeof(props.temperature) === "number" && !isNaN(props.temperature) && <p id='temperature' className="Temperature">{props.temperature}°C</p>}
             </div>
