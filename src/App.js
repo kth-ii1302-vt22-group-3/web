6554db4b11982667b3ee8f97fdd12e873ef450aa
carrogross <caroline.liljenstrom@gmail.com>
2022-05-13 10:46:57 +0200

    #34: Initialize ShowPresenter instead of HomePagePresenter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
 import './App.css';
 import Model from './model/model';
-import HomePagePresenter from './presenters/homePagePresenter';
-import TemperatureView from "./views/temperatureView";
-import TemperatureGraphView from "./views/temperatureGraphView";
-import SidebarView from "./views/sidebarView";
+import ShowPresenter from './presenters/showPresenter';
 
 
 function App() {
   const model = new Model();
   model.getLatest();
   model.getLatests();
+  
   return (
     <div className="App">
-      <BrowserRouter>
-      <SidebarView />
-        <Routes>
-          <Route path="/" element={<Outlet/>}>
-            <Route index path="today" element={<TemperatureView temperature={0} timestamp={"dofhgjl"} />}/>
-            <Route path="graph" element={<TemperatureGraphView chartData={model.chartData} />}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
-      {/* <HomePagePresenter model = {model}/> */}
+      <ShowPresenter model = {model}/>
     </div>
   );
 }
---
+++ src/presenters/showPresenter.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+import TemperatureView from "../views/temperatureView";
+import TemperatureGraphView from "../views/temperatureGraphView";
+import SidebarView from "../views/sidebarView";
+
+function ShowPresenter(props) {
+    const [temper, setTemper] = React.useState(props.model.temperature);
+    const [dateTime, setDateTime] = React.useState(props.model.timestamp);
+    const [chartData, setChartData] = React.useState(props.model.chartData);
+
+    React.useEffect(
+        function () {
+        // add observer to model
+            props.model.addObserver(function () {
+            setTemper(props.model.temperature);
+            setDateTime(props.model.timestamp);
+            setChartData(props.model.chartData);
+        });
+    },
+    [props.model]
+);
+
+return (
+        <>
+            <BrowserRouter>
+            <SidebarView />
+                <Routes>
+                    <Route path="/" element={<Outlet/>}>
+                        <Route index path="today" element={<TemperatureView temperature={111} timestamp={"TEST DATE TIME"} />}/>
+                        <Route path="graph" element={<TemperatureGraphView chartData={props.model.chartData} />}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter></>
+    );
+}
+
+export default ShowPresenter;
